9a16d8756d45bd0d789bd4e8a61bea45d29211d7
Estefania Sanchez Sanchez <faniaeste92@gmail.com>
2026-02-01 13:25:57 +0100

    Agrego más prints, e intento que funcione con un input

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import requests as consulta
 
 #https://restcountries.com
 
-response = consulta.get('https://restcountries.com/v3.1/name/spain')
+
+
+name = input("Introduce el País en Ingles:")
+response = consulta.get('https://restcountries.com/v3.1/name/{name}')
 respuesta = response.json()
 print("Nombre del pais: ",respuesta[0]['name'])
+print("Moneda: ", respuesta[0]['currencies'])
+print("Capital", respuesta[0]['capital'])
+print("Lenguas", respuesta[0]['languages'])
